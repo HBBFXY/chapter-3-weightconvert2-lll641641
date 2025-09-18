1e87835b85eec360638dc1f001a0f61123b6422b
lll641641 <1624823256@qq.com>
2025-09-18 22:32:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # 在这个文件里编写代码
+weight = float(input())
+increase = 0.5
+moon_ratio = 0.165
+
+for year in range(1, 11):
+    earth = weight + increase * year
+    moon = earth * moon_ratio
+    print(f"{year} {earth:.1f} {moon:.2f}")
